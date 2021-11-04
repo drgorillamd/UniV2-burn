f9f198ef137f1fb915ad95579e93bd2ed5d224fc
drgorillamd <dr_gorilla@protonmail.com>
2021-11-04 22:15:41 +0100

    async/for to fix

--- test.js
+++ test.js
@@ -13,45 +13,24 @@ const UNI2_FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'; //deployed: 1
 // Flashbots provider requires passing in a standard provider and an auth signer
 
 const main = async function () {
-    //provider = new ethers.providers.WebSocketProvider(process.env.PROV);
-    provider = new ethers.providers.JsonRpcProvider(process.env.HTTP_PROV);
-    //provider = new ethers.providers.WebSocketProvider(process.env.MORALIS_PROV);
-    const abi = ["event PairCreated(address indexed token0, address indexed token1, address pair, uint)",
-                    "function allPairsLength() external view returns (uint)"]
+    provider = new ethers.providers.WebSocketProvider(process.env.PROV);
+
+    const abi = ["event PairCreated(address indexed token0, address indexed token1, address pair, uint)"];
     const factory = new ethers.Contract(UNI2_FACTORY, abi, provider);
-
     const filter = factory.filters.PairCreated();
-    console.log(filter);
 
-    provider.resetEventsBlock(10000835);
+    const step = 500;
+    let query;
 
-    //const query = await factory.queryFilter(filter);
-    const query = await provider.getLogs(filter);
-    console.log(query);
-
-/*
-    factory.on('PairCreated', (token0, token1, pair) => {
-        console.log(token0+" x "+token1+" @ "+pair);
-    })
-    */
-
-/*
-    provider.on("pending", async (tx) => {
-        console.log(tx);
-    });
-*/
-    provider._websocket.on("error", async () => {
-        console.log(`Unable to connect, retrying in 3s...`);
-        setTimeout(init, 3000);
-    }); 
-    
-    provider._websocket.on("close", async (code) => {
-        console.log(
-        `Connection lost with code ${code}! Attempting reconnect in 3s...`
-        );
-        provider._websocket.terminate();
-        setTimeout(init, 3000);
-    });
+    for(let from = 10000835; from < 13541900; from+=step) {
+        console.log("batch "+from+" to "+(from+step));
+        query = await factory.queryFilter(filter, 13541381, from+step);
+        for(let i=0; i<query.length; i++) {
+            const adr = query[i].args.pair;
+            const tmp = new ethers.Contract(adr, ["function balanceOf(address owner) external view returns (uint)"], provider);
+            console.log(adr+" : "+await tmp.balanceOf(adr));
+        }
+    }
 
 };
 
@@ -63,16 +42,3 @@ async function process_tx(hash) {
 }
 
 main();
-
-
-
-/*
-const signedBundle = await flashbotsProvider.signBundle([
-    {
-        signer: SOME_SIGNER_TO_SEND_FROM,
-        transaction: SOME_TRANSACTION_TO_SEND
-    }
-])
-
-const bundleReceipt = await flashbotsProvider.sendRawBundle(signedBundle, TARGET_BLOCK_NUMBER)
-*/
