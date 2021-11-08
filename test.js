7e99e37c2fde725556dfafc7150ec635e16e71bf
drgorillamd <dr_gorilla@protonmail.com>
2021-11-09 00:59:25 +0100

    0

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const ethers = require('ethers');
+const async = require('async');
 require('dotenv').config();
-const { FlashbotsBundleProvider} = require('@flashbots/ethers-provider-bundle');
-let provider;
+
+//const { FlashbotsBundleProvider} = require('@flashbots/ethers-provider-bundle');
+
 
 const UNI2_FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'; //deployed: 10000835 
 
@@ -13,26 +15,56 @@ const UNI2_FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'; //deployed: 1
 // Flashbots provider requires passing in a standard provider and an auth signer
 
 const main = async function () {
-    provider = new ethers.providers.WebSocketProvider(process.env.PROV);
-
+    const provider = new ethers.providers.WebSocketProvider(process.env.PROV);
     const abi = ["event PairCreated(address indexed token0, address indexed token1, address pair, uint)"];
     const factory = new ethers.Contract(UNI2_FACTORY, abi, provider);
     const filter = factory.filters.PairCreated();
-
-    const step = 500;
-    let query;
-
+/*
     for(let from = 10000835; from < 13541900; from+=step) {
         console.log("batch "+from+" to "+(from+step));
-        query = await factory.queryFilter(filter, 13541381, from+step);
+        query = await factory.queryFilter(filter, from, from+step);
+
+
         for(let i=0; i<query.length; i++) {
             const adr = query[i].args.pair;
             const tmp = new ethers.Contract(adr, ["function balanceOf(address owner) external view returns (uint)"], provider);
             console.log(adr+" : "+await tmp.balanceOf(adr));
         }
     }
-
+*/
+    const step = 10;
+    let from = 10000835;
+    let query;
+    async.whilst
+    (
+        function() {return from < 13541900;},
+        function(outer_cb)
+        {
+            let to = from+step;
+            console.log("from "+from+" to "+to);
+            let j = 0;
+            query = factory.queryFilter(filter, from, to);
+            async.whilst
+            (
+                function () { return j < query.length; },
+                function (inner_cb) 
+                {
+                    const adr = query[i].args.pair;
+                    const tmp = new ethers.Contract(adr, ["function balanceOf(address owner) external view returns (uint)"], provider);
+                    console.log(adr+" : "+tmp.balanceOf(adr));
+                    setTimeout(inner_cb, 0);
+                },
+                function () 
+                {
+                    from+=step;
+                    outer_cb();
+                }
+            );
+        }
+    );
 };
+
+
 
 async function process_tx(hash) {
     for(let i=0; i<=10; i++) {
