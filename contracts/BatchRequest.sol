160a6d647429180c29f29463d81432620d9a4836
DrGorilla_md <83670532+drgorillamd@users.noreply.github.com>
2022-11-24 22:41:57 +0100

    Update BatchRequest.sol

--- contracts/BatchRequest.sol
+++ contracts/BatchRequest.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 interface IFactory {
     function allPairs(uint256 idx) external returns (address);
     function allPairsLength() external view returns (uint);
@@ -12,20 +10,28 @@ interface IERC20 {
     function balanceOf(address adr) external returns (uint256);
 }
 
+/**
+ @dev This contract is not meant to be deployed. Instead, use a static call with the
+      deployment bytecode as payload.
+ */
 contract BatchRequest {
-    constructor(uint256 from, uint256 step, address factory) {
 
+    constructor(uint256 from, uint256 step, address factory) {
+        
+        // There is a max number of pool as a too big returned data times out the rpc
         uint256[] memory returnData = new uint256[](step);
 
         for(uint256 i = 0; i < step; i++) {
             address curr = IFactory(factory).allPairs(from+i);
             returnData[i] = IERC20(curr).balanceOf(curr);
         }
-
+    
         bytes memory data = abi.encode(returnData);
-
+        
+        // This is the way to return data from a constructor
         assembly {
             return(add(data, 64), add(mul(step, 32), 32))
         }
     }
+    
 }
